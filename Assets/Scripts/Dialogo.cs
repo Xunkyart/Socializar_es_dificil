17893cf7428c0f0d106e030dc8deecac7f407a28
Xunkyart <xunky.art@gmail.com>
2026-05-08 13:51:08 +0200

    Mejora del Sistema

--- Assets/Scripts/Dialogo.cs
+++ Assets/Scripts/Dialogo.cs
@@ -7,30 +7,39 @@ using Unity.VisualScripting;
 public class Dialogo : MonoBehaviour
 {
     TextMeshProUGUI objDialogo;
-    string frase = "Hola buenas tardes";
+    public string frase = "Hola buenas tardes";
+    bool escribiendo = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         objDialogo = this.GetComponent<TextMeshProUGUI>();
-        escribirEnPantalla();
+        objDialogo.text = " ";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (escribiendo == false)
+        {
+            escribirEnPantalla();
+        }
     }
 
+    //ESCRIBIR EN PANTALLA  
+    //Este método escribe la variable "frase" con un poco de delay por letra. Borra todo tras 3 seg.
     public void escribirEnPantalla()
     {
+        escribiendo = true;
         StartCoroutine (EscribirLento());
         IEnumerator EscribirLento(){
-            objDialogo.text = " ";
             foreach (char c in frase)
             {
                 objDialogo.text += c;
                 yield return new WaitForSeconds(0.2f);
             }
+            yield return new WaitForSeconds(3.0f);
+            objDialogo.text = " ";
+            escribiendo = false;
         }
     }
 }
