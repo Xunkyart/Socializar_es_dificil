9ea455da58601f4d9a7a15deab7bba7028c76ac2
atromcon <atromcon@gmail.com>
2026-05-15 12:12:21 +0200

    Commit de sonido con slider de musica funcional

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource FXSource;
+
+    //public AudioClip
+    public AudioClip Prueba;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musicSource.clip = Prueba;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    public void PlaySFX(AudioClip clip)
+    {
+        FXSource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetVolume(); 
+
+    }
+
+    // Update is called once per frame
+    public void SetVolume()
+    {
+        float volume = volumeSlider.value;
+        audioMixer.SetFloat("Music", volume);
+       
+    }
+
+    void Update()
+    {
+        SetVolume();
+    }
+        
+    }
+
