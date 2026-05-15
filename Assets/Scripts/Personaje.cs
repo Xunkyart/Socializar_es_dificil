d215dfec7f5f20bfe1a7e4d7d95f6052537a8d1c
Xunkyart <xunky.art@gmail.com>
2026-05-15 13:56:57 +0200

    pa casa

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -1,7 +1,23 @@
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
+    //  PICKERS
+    public GameObject cejaIZq;
+    public GameObject ceraDcha;
+    public GameObject labioInf;
+    public GameObject dientesInf;
+    public GameObject comisuraIzq;
+    public GameObject comisuraDcha;
+    //SLIDERS
+    public GameObject sliderCejaIZq;
+    public GameObject SliderCeraDcha;
+    public GameObject SliderAperturaBoca;
+    public GameObject SliderComisuraIzq;
+    public GameObject SliderComisuraDcha;
+    public float valor = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,6 +26,46 @@ public class Personaje : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Sacar el valor del slider
+    public void getSliderCejaIzq()
+    {
+        
+    }
+
+        public void getSliderCejaDcha()
+    {
+        
+    }
+
+        public void getSliderAperturaBoca()
+    {
+        
+    }
+
+        public void getSliderComisuraIzq()
+    {
+        
+    }
+
+        public void getSliderComisuraDcha()
     {
         
     }
