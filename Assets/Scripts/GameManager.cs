c20a83db6cce318fddb3bcad334727a80189b79f
LucasFerre777 <lucassferre04@gmail.com>
2026-05-25 15:07:46 +0200

    lo he dejado preparado para empezar en casa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*cuando llegue a casa: 
+Poner botones de volver a jugar y salir del juego
+Mirar como conectar al resto del juego
+
+Duplicar script de dialogo para hacer pruebas con lo que hice ayer y con lo de ahora
+*/
